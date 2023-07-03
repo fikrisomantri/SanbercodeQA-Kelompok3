338b6edc52aad176dc188783259374e0731865e1
Azzania Fitria <137741757+Azzania@users.noreply.github.com>
2023-07-03 09:10:14 +0700

    Delete sign-up.cy.js

--- cypress/e2e/4-finalproject/sign-up.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Sign Up Demoblaze', () => {
-  it('Success Sign Up', () => {
-    cy.visit('https://www.demoblaze.com/index.html')
-    cy.get('#sign-username')
-    cy.get('#sign-password')
-    cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary')
-    cy.wait(100)
-  }) 
-})
